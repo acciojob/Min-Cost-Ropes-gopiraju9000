b47c6a5181b5aa0618d02fc10f57ebec3273ce9d
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2024-09-02 12:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function mincost(arr)
-{ 
+function mincost(arr){ 
 //write your code here
 // return the min cost
-  
+  arr.sort((a,b) => a-b);
+  let min_sum = arr[0];
+   for(int i = 1;i < arr.length;i++){
+	   min_sum = min_sum  + arr[i];
+   }
+	return min_sum;
 }
 
 module.exports=mincost;
