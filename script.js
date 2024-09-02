f079a4d26766814d3f7bc0113e63d90cccd546f4
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2024-09-02 12:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function mincost(arr){
 //write your code here
 // return the min cost
   arr.sort((a,b) => a-b);
-  let min_sum = arr[0];
+  let min_sum = arr.length == 1 ? 0 : arr[0];
    for(let i = 1;i < arr.length;i++){
 	   min_sum = min_sum  + arr[i];
    }
